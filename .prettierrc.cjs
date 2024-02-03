adb47661b96eb14182012bda72175722a3669c15
José Silva <jasilva15@gmail.com>
2024-02-03 12:47:51 -0500

    style: :rotating_light: eslint and prettier are instelled and configured

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tabWidth: 2, // Ancho del tab es 2
+  semi: true, // Uso de ; obligatorio
+  singleQuote: true, // uso de comillas simples obligatorio
+  useTabs: false, // Espacios de identación no son de tipo tab.
+  trailingComma: "all", // Agregar una coma al definir la última propiedad de un arreglo.
+};
